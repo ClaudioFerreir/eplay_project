40a110116e5cb9d36048ac8ad4f8bd225e3e923b
Claudio Ferreira Junior <claudioferreirajr@hotmail.com>
2024-02-08 22:42:58 -0300

    feat: header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,33 @@
+import { HeaderBar, LinkCart, LinkItem, Links } from './styles'
+
+import logo from '../../assets/images/logo.svg'
+import carrinho from '../../assets/images/carrinho.svg'
+
+const Header = () => (
+  <HeaderBar>
+    <div>
+      <img src={logo} alt="EPLAY" />
+      <nav>
+        <Links>
+          <LinkItem>
+            <a href="#">Categorias</a>
+          </LinkItem>
+          <LinkItem>
+            <a href="#">Novidades</a>
+          </LinkItem>
+          <LinkItem>
+            <a href="#">Promoções</a>
+          </LinkItem>
+        </Links>
+      </nav>
+    </div>
+    <div>
+      <LinkCart href="#">
+        0 - produtos
+        <img src={carrinho} alt="Carrinho" />
+      </LinkCart>
+    </div>
+  </HeaderBar>
+)
+
+export default Header
