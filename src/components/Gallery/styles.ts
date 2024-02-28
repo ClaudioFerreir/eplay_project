a7be55bc022707ad39407ce1a6c2c315ae8c08d5
Claudio Ferreira Junior <claudioferreirajr@hotmail.com>
2024-02-28 17:34:53 -0300

    feat: task 3 ended

--- src/components/Gallery/styles.ts
+++ src/components/Gallery/styles.ts
@@ -3,6 +3,10 @@ import { cores } from '../../styles'
 
 export const Items = styled.ul`
   display: flex;
+  gap: 16px;
+  flex-wrap: wrap;
+  max-width: 100%;
+  width: 100%;
 `
 
 export const Action = styled.div`
@@ -20,7 +24,6 @@ export const Action = styled.div`
 `
 
 export const Item = styled.li`
-  margin-right: 16px;
   position: relative;
 
   > img {
--- src/components/ProductsList/styles.ts
+++ src/components/ProductsList/styles.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 
-import { cores } from '../../styles'
+import { breakpoints, cores } from '../../styles'
 import { Card } from '../Product/styles'
 
 type Props = {
@@ -23,6 +23,14 @@ export const List = styled.ul`
   grid-template-columns: 1fr 1fr 1fr 1fr;
   gap: 24px;
   margin-top: 40px;
+
+  @media (max-width: ${breakpoints.desktop}) {
+    grid-template-columns: 1fr 1fr;
+  }
+
+  @media (max-width: ${breakpoints.tablet}) {
+    grid-template-columns: 1fr;
+  }
 `
 
 export const Title = styled.h2`
